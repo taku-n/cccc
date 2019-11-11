65dfcd7c12e7445d71b3264a29be62b1c027aab0
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2019-11-11 11:20:50 +0000

    use volumes

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
+echo "---- init.sh begins ----"
+
 nginx
 
 if [ $STAGE = production ]; then
+  echo "Trying to get production level certificates..."
   certbot certonly --webroot \
       -n -t --agree-tos -m $EMAIL -w /usr/share/nginx/html -d $DOMAIN
 else
+  echo "Trying to get staging level certificates..."
   certbot certonly --webroot --test-cert \
       -n -t --agree-tos -m $EMAIL -w /usr/share/nginx/html -d $DOMAIN
 fi
@@ -12,3 +16,5 @@ envsubst '$DOMAIN' < /etc/nginx/conf.d/cccc.conf.template > /etc/nginx/conf.d/cc
 nginx -s reload
 
 crond -f
+
+echo "----  init.sh ends  ----"
